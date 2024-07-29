6c54a403a5841fd4952de889ee3c9e31ebcd6022
Marvin <corro.marvin@gmail.com>
2024-07-28 21:49:41 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express"
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
